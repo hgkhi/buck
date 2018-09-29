e583d14b8b783dba563add94dbe97d66d3310df5
Hoang, Khoi a | Joseph | ECMD <khoi.a.hoang@rakuten.com>
2018-09-29 20:11:06 +0900

    Implemented OutsideBucket

---
+++ src/buck/OutsideBucket.java
@@ -0,0 +1,12 @@
+package buck;
+
+/**
+ *
+ * @author khoi.a.hoang
+ */
+public class OutsideBucket extends AbstractBucket {
+    
+    public OutsideBucket(String bucketId) {
+        super(bucketId);
+    }
+}
